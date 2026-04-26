1e28ffd49a90b9493232716241f37f11e2ce38ed
Le Dinh Duc <ledinhduc1879@gmail.com>
2026-04-26 15:02:03 +0700

    chore: remove tracked dbt target folder

--- dbt/thelook_dwh/target/compiled/thelook_dwh/models/staging/stg_distribution_centers.sql
+++
@@ -1,8 +0,0 @@
-
-
-select
-    cast(id as int64) as distribution_center_id,
-    cast(name as string) as distribution_center_name,
-    cast(latitude as float64) as latitude,
-    cast(longitude as float64) as longitude
-from `cloud-data-project-492514`.`thelook_staging`.`dist_centers`
